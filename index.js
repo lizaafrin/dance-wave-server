6f54f659917d81ccd6756dae80b401b068979b1d
lizaafrin <lizaafrin446@gmail.com>
2023-06-10 22:53:26 +0600

    jwt token verified

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
+
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
@@ -9,9 +11,25 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+      return res.status(401).send({ error: true, message: 'Unauthorized access' });
+  }
+  // bearer token
+  const token = authorization.split(' ')[1];
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+      if (err) {
+          res.status(401).send({ error: true, message: 'Unauthorized access' });
+      }
+      req.decoded = decoded;
+      next();
+  })
+}
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qupsx4j.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -28,10 +46,21 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+
+
     const danceCollection = client.db("danceWaveDB").collection("danceClasses");
     const usersCollection = client.db("danceWaveDB").collection("users");
     const selectedClassCollection = client.db("danceWaveDB").collection("selectedClass");
-    
+
+    // jwt token api
+    app.post('/jwt', (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1hr' })
+
+      res.send({ token })
+    });
+
+
     // API for all classes
     app.get('/danceclasses', async (req, res) => {
       const result = await danceCollection.find().toArray();
@@ -54,19 +83,75 @@ async function run() {
       const result = await usersCollection.insertOne(user);
       res.send(result);
     })
-     
+
+    app.patch('/users/admin/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: 'admin',
+        },
+      };
+
+      const result = await usersCollection.updateOne(query, updateDoc);
+      res.send(result);
+    });
+
+    // app.patch('/users/instructor/:id', async (req, res) => {
+    //   const id = req.params.id;
+    //   const query = { _id: new ObjectId(id) };
+    //   const updateDoc = {
+    //     $set: {
+    //       role: 'instructor', 
+    //     },
+    //   };
+
+    //   const result = await usersCollection.updateOne(query, updateDoc);
+    //   res.send(result);
+    // });
+
+    app.delete('/users/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await usersCollection.deleteOne(query);
+      res.send(result);
+    });
+
     // selected class api
-    app.post('/selectedclass', async(req,res) => {
-      const item = req.body;
-      const query = { name: item.name }
-      const existingClass = await selectedClassCollection.findOne(query);
-      if(existingClass){
-        return res.send({message: 'Class already selected'})
+    app.get('/selectedclass',verifyJWT, async (req, res) => {
+      const email = req.query.email;
+      if (!email) {
+        res.send([]);
       }
+      const decodedEmail = req.decoded.email;
+      if (email !== decodedEmail) {
+          return res.status(403).send({ error: true, message: 'Forbidden access' })
+      }
+        const query = { email: email };
+        const result = await selectedClassCollection.find(query).toArray();
+        res.send(result);
+      // const result = await selectedClassCollection.find().toArray();
+      // res.send(result);
+    })
+
+
+    app.post('/selectedclass', async (req, res) => {
+      const item = req.body;
+      // const query = { name: item.name }
+      // const existingClass = await selectedClassCollection.findOne(query);
+      // if (existingClass) {
+      //   return res.send({ message: 'Class already selected' })
+      // }
       // console.log(item);
       const result = await selectedClassCollection.insertOne(item);
       res.send(result);
     })
+    app.delete('/selectedclass/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await selectedClassCollection.deleteOne(query);
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
